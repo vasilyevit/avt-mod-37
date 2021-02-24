ff245d19e7623055572bdeb03db2eb5e58159ed5
avasilyev <itvaa@ya.ru>
2021-02-24 19:27:36 +0300

    Тест 2

--- JavaAppiumAutomation/src/MainClass.java
+++ JavaAppiumAutomation/src/MainClass.java
@@ -3,4 +3,10 @@ public class MainClass {
     public Integer getLocalNumber(){
         return 14;
     }
+
+    private Integer class_number = 20;
+
+    public Integer getClassNumber(){
+        return class_number;
+    }
 }
--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -7,4 +7,9 @@ public class MainClassTest {
     public void testGetLocalNumber(){
         Assert.assertEquals("getLocalNumber вернул число отличное от 14", 14, (int) new MainClass().getLocalNumber());
     }
+
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("getClassNumber вернуло значение меньше 45", (int) new MainClass().getClassNumber() > 45);
+    }
 }
